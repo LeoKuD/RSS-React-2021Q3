2b00169f72a1f9e5b7c72a48d371388f062c24d0
leokud <then7w@gmail.com>
2021-08-18 18:40:58 +0300

    fix: eslint errors, width elements

--- src/data.js
+++ src/data.js
@@ -1,28 +1,26 @@
-const api_key = '&api_key=a30169317af24374d97e8da29c8914e7'
-const per_page = '&per_page='
+const apiKey = '&api_key=a30169317af24374d97e8da29c8914e7'
+const perPage = '&per_page='
 const pageNumb = '&page='
 const format = '&format=json&nojsoncallback=1'
 const text = '&text='
-const min_upload_date = '&min_upload_date='
-const max_upload_date = '&max_upload_date='
+const minUploadDate = '&min_upload_date='
+const maxUploadDate = '&max_upload_date='
 const sorti = '$sort='
 import { instance } from './services/api'
 
 export const getPhotos = async (
   method,
   param,
-  min_date,
-  max_date,
+  minDate,
+  maxDate,
   sort,
   page,
   per
 ) => {
   const response = await instance.get(
-    `${method}${api_key}${text + param}${
-      min_date && min_upload_date + min_date
-    }${max_date && max_upload_date + max_date}${sort && sorti + sort}${
-      pageNumb + page
-    }${per_page + per}${format}_w`
+    `${method}${apiKey}${text + param}${minDate && minUploadDate + minDate}${
+      maxDate && maxUploadDate + maxDate
+    }${sort && sorti + sort}${pageNumb + page}${perPage + per}${format}_w`
   )
   return response
 }
